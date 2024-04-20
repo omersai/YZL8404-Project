b8acab2db4707a12a7ba516b3c5fd51afb035a4c
Ömer Saitoğlu <omer.saitoglu>
2024-04-20 12:04:40 +0300

    Added - Project Files

---
+++ YZL8404-Project/YZL8404-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YZL8404-Project
+//
+//  Created by Ömer Saitoğlu on 20.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
