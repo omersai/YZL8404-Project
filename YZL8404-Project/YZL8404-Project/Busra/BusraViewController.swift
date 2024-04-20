d84129b3e32efef412656181f4fae49c129ce866
Ebusra <github.com/Ebusra>
2024-04-20 13:04:52 +0300

    Added-BusraVC

---
+++ YZL8404-Project/YZL8404-Project/Busra/BusraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BusraViewController.swift
+//  YZL8404-Project
+//
+//  Created by Emine Büşra Yıldız on 20.04.2024.
+//
+
+import UIKit
+
+class BusraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
