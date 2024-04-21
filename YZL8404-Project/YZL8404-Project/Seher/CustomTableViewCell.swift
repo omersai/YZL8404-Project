dc87da6d6d120f07e8132229ccb32a0325f115f7
Seher Badır <pdseher@gmail.com>
2024-04-21 12:57:22 +0300

    Added-CustomCell

---
+++ YZL8404-Project/YZL8404-Project/Seher/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  YZL8404-Project
+//
+//  Created by Seher Badır on 21.04.2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
