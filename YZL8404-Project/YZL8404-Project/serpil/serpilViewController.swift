eac2af8b89909571d00ea99e189a9928e3544f8d
serpil sunter <>
2024-04-21 12:02:14 +0300

    added TableView

--- YZL8404-Project/YZL8404-Project/serpil/serpilViewController.swift
+++ YZL8404-Project/YZL8404-Project/serpil/serpilViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class serpilViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    struct Film{
+        var adi: String
+        var aciklama: String
+        var yili: Int
+    }
+    class CustomTableViewController: UIViewController {
+        @IBOutlet var label: UILabel!
+        
+        @IBOutlet var tableView: UITableView!
+        
+        var filmler: [Film] = []
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Do any additional setup after loading the view.
+        }
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
