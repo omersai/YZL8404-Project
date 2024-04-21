b5814a841e4dc3aa2317f445aca3f4724a02c8f2
İsmail Kocaoğlu <ismailkocaogluu.27@gmail.com>
2024-04-21 12:03:31 +0300

    Added-IsmailTableView
    i added custom tableView

---
+++ YZL8404-Project/YZL8404-Project/Ismail/InformationModel.swift
@@ -0,0 +1,15 @@
+//
+//  InformationModel.swift
+//  YZL8404-Project
+//
+//  Created by İsmail Kocaoglu on 21.04.2024.
+//
+
+import Foundation
+
+struct InformationModel {
+    var name : String
+    var surname : String
+    
+  
+}
